092b25e295efc57be3a1a8414f494c744ddda8a1
Stanislav-Karasyk <99.kara.99@gmail.com>
2020-12-14 21:51:48 +0200

    start + task-1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const name = 'Генератор защитного поля';
+let price = 1000;
+
+const result = `Выбран "${name}", цена за штуку ${price} кредитов`
+
+console.log(result);
+
+price = 2000;
+
+const result2 = `Выбран "${name}", цена за штуку ${price} кредитов`
+
+console.log(result2);
