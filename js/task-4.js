4007aa47ebf9b48c0b2a88439f09e9ee4ed1d73e
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-01-03 11:13:19 +0200

    ref

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,22 @@
-'use strict';
+// 'use strict';
 
-let credits = 23580;
-const pricePerDroid = 3000;
-let totalPrice;
+// let credits = 23580;
+// const pricePerDroid = 3000;
+// let totalPrice;
 
-let userInput = prompt('Сколько дроидов Вы хотите купить?');
-userInput = Number(userInput)
+// let userInput = prompt('Сколько дроидов Вы хотите купить?');
+// userInput = Number(userInput)
 
-if (!userInput) {
-    alert('Отменено пользователем!');
+// if (!userInput) {
+//     alert('Отменено пользователем!');
 
-}  else if ( pricePerDroid * userInput > credits) {
-    alert('Недостаточно средств на счету!');
+// }  else if ( pricePerDroid * userInput > credits) {
+//     alert('Недостаточно средств на счету!');
 
-} else {
-    totalPrice = pricePerDroid * userInput;
-    credits -= totalPrice;
-    alert(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`)
-}
+// } else {
+//     totalPrice = pricePerDroid * userInput;
+//     credits -= totalPrice;
+//     alert(`Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`)
+// }
+
+
